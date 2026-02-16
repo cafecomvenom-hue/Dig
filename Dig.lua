ac073773dbea614a1483aa928e4983d85310c6f5
cafecomvenom-hue <cafecomvenom@gmail.com>
2026-02-16 20:41:45 -0300

    Create Dig.lua with warning for unverified script
    Add warning comment for unverified script usage.

---
+++ Dig.lua
@@ -0,0 +1,7 @@
+--[[
+	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+]]
+game:GetService("Players").LocalPlayer
+	.ShovelFlags
+	.AdminShovel
+	.Value = true
